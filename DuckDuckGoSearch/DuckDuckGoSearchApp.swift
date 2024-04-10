2aaa77f333ad5446157e6522e9566d6d9e8e03e1
daniel ortiz millan <danielortizmillan@MacBook-Air-de-daniel.local>
2024-04-10 15:29:46 -0600

    first commit

---
+++ DuckDuckGoSearch/DuckDuckGoSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuckDuckGoSearchApp.swift
+//  DuckDuckGoSearch
+//
+//  Created by daniel ortiz millan on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DuckDuckGoSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
